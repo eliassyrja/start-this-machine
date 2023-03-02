d5dee6d19a40c028f2850892c100a8f06f376b9a
Elias <elias.syrja@gmail.com>
2023-03-02 18:05:59 +0200

    Add second camera for inspecting objects
    Changes to InspectableObject script. Added second camera that is used for object inspection.
    Script still needs work, as object can't be interacted with after first inspection.
    Also Main Camera is giving some errors while in inspection mode! :alien:

--- Assets/Code/InspectableObject.cs
+++ Assets/Code/InspectableObject.cs
@@ -11,13 +11,17 @@ public class InspectableObject : MonoBehaviour
     //GameObject that this script is assigned to, clone of the object will be inspected.
     [SerializeField] private GameObject inspectedObject;
 
+    //Cameras for easier handling in Unity Editor.
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Camera inspectionCamera;
+
     //Scale of inspected object.
     [SerializeField] private float inspectedItemScaleChange;
 
     //Speed of object rotation.
     [SerializeField] private float inspectionSpeed;
     [SerializeField] private bool inspectionActive = false;
-
+    
     private GameObject originalObject;
 
     public void OnMouseOver()
@@ -57,7 +61,6 @@ public class InspectableObject : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             EndInspection();
-            originalObject.SetActive(true);
         }
     }
 
@@ -65,20 +68,26 @@ public class InspectableObject : MonoBehaviour
     {
         print("SETUP INSPECTION CALLED");
         inspectionActive = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
         originalObject = gameObject;
-        originalObject.SetActive(false);
 
         inspectedObject = Instantiate(originalObject, Camera.main.transform.position, new Quaternion(0, 0, 0, 0));
         inspectedObject.SetActive(true);
         inspectedObject.transform.position += new Vector3(0, 0, 1);
         inspectedObject.transform.localScale += new Vector3(-inspectedItemScaleChange, -inspectedItemScaleChange, -inspectedItemScaleChange);
+
+        mainCamera.enabled = false;
+        inspectionCamera.enabled = true;
     }
 
     private void EndInspection()
     {
         print("END INSPECTION CALLED");
         inspectionActive = false;
-        originalObject.SetActive(true);
+        mainCamera.enabled = true;
+        inspectionCamera.enabled = false;
+
         Destroy(inspectedObject);
     }
 }
