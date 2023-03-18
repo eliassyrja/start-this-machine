bf7aeeec8e34aba66663a9a0dd4be97e1ee94f87
Elias <elias.syrja@gmail.com>
2023-03-18 14:56:00 +0200

    Remove unused import
    Removed unused import UnityEngine.Events

--- Assets/Code/InspectableObject.cs
+++ Assets/Code/InspectableObject.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider))]
 public class InspectableObject : MonoBehaviour
