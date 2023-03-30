f9d77c7bd545effe47953f63263cf4d458cea09a
ieevari <125186933+ieevari@users.noreply.github.com>
2023-03-30 19:58:52 +0300

    Change CameraController to lerp inspection view
    Also changed InspectableObject to use CameraController when moving camera

--- Assets/Code/InspectableObject.cs
+++ Assets/Code/InspectableObject.cs
@@ -5,96 +5,97 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class InspectableObject : MonoBehaviour
 {
-    //Interaction button.
-    [SerializeField] private KeyCode interactionKey = KeyCode.Mouse0;
+	//Interaction button.
+	[SerializeField] private KeyCode interactionKey = KeyCode.Mouse0;
 
-    //Scale of inspected object.
-    [SerializeField] private float inspectedItemScaleChange;
+	//Scale of inspected object.
+	[SerializeField] private float inspectedItemScaleChange;
 
-    //Speed of object rotation.
-    [SerializeField] private float inspectionSpeed;
+	//Speed of object rotation.
+	[SerializeField] private float inspectionSpeed;
 
-    private GameController gameController;
-    private StateMachine stateMachine;
-    private GameObject inspectedObject;
+	private GameController gameController;
+	private StateMachine stateMachine;
+	private GameObject inspectedObject;
+	private CameraController cameraController;
 
-    private Vector3 startingPosition;
-    private Quaternion startingRotation;
+	private Vector3 startingPosition;
+	private Quaternion startingRotation;
 
-    public void Start()
-    {
-        gameController = FindObjectOfType<GameController>();
-        stateMachine = FindObjectOfType<StateMachine>();
-    }
+	public void Start()
+	{
+		gameController = FindObjectOfType<GameController>();
+		stateMachine = FindObjectOfType<StateMachine>();
+		cameraController = FindObjectOfType<CameraController>();
+	}
 
-    public void Update()
-    {
-        if (stateMachine.GetCurrentState() == StateMachine.State.Inspection)
-        {
-            if (Input.GetKeyDown(KeyCode.E) && gameObject == inspectedObject)
-            {
-                EndInspection();
-            }
-        }
-    }
-    public void OnMouseOver()
-    {
-        if (stateMachine.GetCurrentState() == StateMachine.State.FreeLook)
-        {
-            if (Input.GetKeyDown(interactionKey))
-            {
-                SetupInspection();
-            }
-        }
-    }
+	public void Update()
+	{
+		if (stateMachine.GetCurrentState() == StateMachine.State.Inspection)
+		{
+			if (Input.GetKeyDown(KeyCode.E) && gameObject == inspectedObject)
+			{
+				EndInspection();
+			}
+		}
+	}
+	public void OnMouseOver()
+	{
+		if (stateMachine.GetCurrentState() == StateMachine.State.FreeLook)
+		{
+			if (Input.GetKeyDown(interactionKey))
+			{
+				SetupInspection();
+			}
+		}
+	}
 
-    public void OnMouseDrag()
-    {
-        if (stateMachine.GetCurrentState() == StateMachine.State.Inspection)
-        {
-            InspectItem();
-        }
-    }
+	public void OnMouseDrag()
+	{
+		if (stateMachine.GetCurrentState() == StateMachine.State.Inspection)
+		{
+			InspectItem();
+		}
+	}
 
-    private void InspectItem()
-    {
-        if (Input.GetKey(interactionKey))
-        {
-            var horizontal = Input.GetAxis("Mouse X") * inspectionSpeed;
-            var vertical = Input.GetAxis("Mouse Y") * inspectionSpeed;
+	private void InspectItem()
+	{
+		if (Input.GetKey(interactionKey))
+		{
+			var horizontal = Input.GetAxis("Mouse X") * inspectionSpeed;
+			var vertical = Input.GetAxis("Mouse Y") * inspectionSpeed;
 
-            gameObject.transform.Rotate(vertical, -horizontal, 0, Space.World);
-        }
-    }
+			gameObject.transform.Rotate(vertical, -horizontal, 0, Space.World);
+		}
+	}
 
-    private void SetupInspection()
-    {
-        Debug.Log("SetupInspection called.");
-        stateMachine.ChangeState(StateMachine.State.Inspection);
+	private void SetupInspection()
+	{
+		Debug.Log("SetupInspection called.");
+		stateMachine.ChangeState(StateMachine.State.Inspection);
 
-        inspectedObject = gameObject;
+		inspectedObject = gameObject;
 
-        gameController.ShowCursor();
-        Debug.Log("Starting position before:" + startingPosition);
-        startingPosition = gameObject.transform.position;
-        startingRotation = gameObject.transform.rotation;
-        Debug.Log("Starting position after:" + startingPosition);
+		gameController.ShowCursor();
+		startingPosition = gameObject.transform.position;
+		startingRotation = gameObject.transform.rotation;
 
-        gameObject.transform.position = Camera.main.transform.position + new Vector3(0, 0, 1);
-        gameObject.transform.localScale -= new Vector3(inspectedItemScaleChange, inspectedItemScaleChange, inspectedItemScaleChange);
+		gameObject.transform.position = Camera.main.transform.position + new Vector3(0, 0, 1);
+		gameObject.transform.localScale -= new Vector3(inspectedItemScaleChange, inspectedItemScaleChange, inspectedItemScaleChange);
 
-        Camera.main.transform.rotation = new Quaternion(0, 0, 0, 0);
-    }
+		cameraController.HandleInspectionCamera(true);
+	}
 
-    private void EndInspection()
-    {
-        Debug.Log("EndInspection called.");
+	private void EndInspection()
+	{
+		Debug.Log("EndInspection called.");
 
-        gameController.HideCursor();
-        stateMachine.ChangeState(StateMachine.State.FreeLook);
+		gameController.HideCursor();
+		stateMachine.ChangeState(StateMachine.State.FreeLook);
 
-        inspectedObject.transform.SetPositionAndRotation(startingPosition, startingRotation);
-        inspectedObject.transform.localScale += new Vector3(inspectedItemScaleChange, inspectedItemScaleChange, inspectedItemScaleChange);
-        inspectedObject = null;
-    }
+		inspectedObject.transform.SetPositionAndRotation(startingPosition, startingRotation);
+		inspectedObject.transform.localScale += new Vector3(inspectedItemScaleChange, inspectedItemScaleChange, inspectedItemScaleChange);
+		inspectedObject = null;
+		cameraController.HandleInspectionCamera(false);
+	}
 }
