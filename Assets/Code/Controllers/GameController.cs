195be4e9a82a1057ca2816ba74199bfe1887c5b6
Elias <elias.syrja@gmail.com>
2023-04-02 12:23:01 +0300

    Change menu close handling
    Changed how menu closing is checked when pressing ESC.

--- Assets/Code/Controllers/GameController.cs
+++ Assets/Code/Controllers/GameController.cs
@@ -41,6 +41,10 @@ public class GameController : MonoBehaviour
             {
                 ShowPauseMenu();
             }
+            else if (settingsMenu.enabled == true)
+            {
+                CloseSettingsMenu();
+            }
             else
             {
                 ClosePauseMenu();
